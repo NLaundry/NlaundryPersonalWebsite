ab9b43315b2f31a02cb8192549c5b770d1e51264
Nathan Laundry <nathanlaundry@Nathans-MacBook-Pro.local>
2023-06-10 13:07:33 -0400

    lol colour I guess

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,13 +5,13 @@ module.exports = {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         colors: {
-            primary_colour: '#AD8B5F',
-            primary_active_colour: '#8F6E3D',
-            accent_colour: '#2D6072',
-            background_colour: '#1B1F23',
-            background_card_colour: '#2C3339',
-            background_card_hover_colour: '#3E464C',
-            text_colour: '#e7e5e4',
+            primary_colour: '#C0663D',
+            primary_active_colour: '#A8481B',
+            accent_colour: '#137179',
+            background_colour: '#1C1D1E',
+            background_card_colour: '#1F2427',
+            background_card_hover_colour: '#1F2427',
+            text_colour: '#DCE5E7',
             text_secondary_colour: '#CAC8CE'
 
         },
